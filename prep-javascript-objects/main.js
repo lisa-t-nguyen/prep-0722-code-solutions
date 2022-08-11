21f4fe3b0b0d772a1b9c5032095d23caa1a9bae0
Lisa Nguyen <lisanguyensc@gmail.com>
2022-08-11 20:55:08 +0000

    Completed prep-javascript-objects.

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,15 @@
+var person = {
+  firstName: 'Boopy',
+  lastName: 'Nguyen',
+  hobby: 'Zoomies',
+  job: 'Cat',
+  previousJob: 'Kitten'
+};
+var fullName = "The person's name is " + person.firstName + ' ' + person.lastName + '.';
+var currentJob = "The person's current job is " + person.job + '.';
+var previousJob = "The person's previous job is " + person.previousJob + '.';
+console.log(person);
+console.log(fullName);
+console.log(currentJob);
+console.log(previousJob);
+console.log(person);
