8066864ddc8b795a46e087b321c68e1833d0d736
Lisa Nguyen <lisanguyensc@gmail.com>
2022-08-11 21:13:14 +0000

    Completed prep-javascript-arrays.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,10 @@
+const languages = ['Html', 'CSS', 'JavaScript', 'React', 'Node', 'Postgres'];
+console.log(languages);
+var firstElement = languages[0];
+console.log(firstElement);
+var thirdElement = languages[2];
+console.log(thirdElement);
+var length = (languages.length);
+console.log(length);
+var lastElement = languages[6 - 1];
+console.log(lastElement);
